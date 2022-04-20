39a143949368a23684db794c685c8ebc50d53565
André Fernandes <92834067+andrefernandeslp1@users.noreply.github.com>
2022-04-20 00:37:54 +0000

    Atualizado negativos5

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -6,7 +6,13 @@ const int SIZE = 5; // input size.
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    int i, x, cont= 0;
+
+    for ( i = 0 ; i < SIZE ; i++ ){
+        cin >> x;
+        if( x<0 ){ cont++ ;}
+    }
+    cout << cont;
 
     return 0;
 }
